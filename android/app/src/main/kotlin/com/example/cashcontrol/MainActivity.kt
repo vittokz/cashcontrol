c62a653ba70f7a24c213182c8663128a1a41be3a
jobpersonal <jobocanegra@personalsoft.com.co>
2021-03-02 09:49:53 -0500

    initial setup

---
+++ android/app/src/main/kotlin/com/example/cashcontrol/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cashcontrol
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/myrouter.dart
@@ -0,0 +1,8 @@
+import 'package:cashcontrol/src/pages/home_page.dart';
+import 'package:flutter/material.dart' show WidgetBuilder;
+class MyRouter {
+  static final Map<String, WidgetBuilder> _routes = {
+    '/': (_) => HomePage()
+  };
+  static Map<String, WidgetBuilder> get routes => _routes;
+}
